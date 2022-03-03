7e232c51d4e7651eeaa89a09fe0675bd0d669d69
faruqneo <faruq_khan@yahoo.com>
2022-03-03 11:25:56 +0530

    added logger

--- src/store.js
+++ src/store.js
@@ -3,12 +3,13 @@ import { routerMiddleware } from "connected-react-router";
 import thunk from "redux-thunk";
 import { createBrowserHistory } from "history";
 import rootReducer from "./reducers";
+import logger from 'redux-logger'
 
 export const history = createBrowserHistory();
 
 const initialState = {};
 const enhancers = [];
-const middleware = [thunk, routerMiddleware(history)];
+const middleware = [thunk, routerMiddleware(history), logger];
 
 if (process.env.NODE_ENV === "development") {
   const devToolsExtension = window.__REDUX_DEVTOOLS_EXTENSION__;
